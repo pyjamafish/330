766be1c85db71011c423509e5e4f0ec853c78201
Fisher Sun <fisher521.fs@gmail.com>
2022-06-07 12:00:26 -0400

    Use nested html tags

--- m2/calculator/calculator.php
+++ m2/calculator/calculator.php
@@ -8,33 +8,49 @@
     <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-    <h1>My calculator</h1>
+    <header>
+        <h1>My calculator</h1>
+    </header>
     <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
         <p>
             <!-- Actions -->
-            <input type="radio" name="action" id="add" value="add">
-            <label for="add">Add</label>
+            <label for="add">
+                <input type="radio" name="action" id="add" value="add">
+                Add
+            </label>
 
-            <input type="radio" name="action" id="subtract" value="subtract">
-            <label for="subtract">Subtract</label>
+            <label for="subtract">
+                <input type="radio" name="action" id="subtract" value="subtract">
+                Subtract
+            </label>
 
-            <input type="radio" name="action" id="multiply" value="multiply">
-            <label for="multiply">Multiply</label>
+            <label for="multiply">
+                <input type="radio" name="action" id="multiply" value="multiply">
+                Multiply
+            </label>
 
-            <input type="radio" name="action" id="divide" value="divide">
-            <label for="divide">Divide</label>
+            <label for="divide">
+                <input type="radio" name="action" id="divide" value="divide">
+                Divide
+            </label>
         </p>
 
         <p>
             <!-- Input 1 -->
-            <label for="input1">Input 1: </label>
-            <input type="number" name="input1" id="input1">
+            <label for="input1">
+                Input 1:
+                <input type="number" name="input1" id="input1">
+            </label>
+
         </p>
 
         <p>
             <!-- Input 2 -->
-            <label for="input2">Input 2: </label>
-            <input type="number" name="input2" id="input2">
+            <label for="input2">
+                Input 2:
+                <input type="number" name="input2" id="input2">
+            </label>
+
         </p>
 
         <p>
